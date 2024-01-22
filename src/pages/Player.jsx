cef2f3f1cc98dddb6401c4d0805e3cabee0cd1a8
Aditya-Nagpal <adityanagpal10@gmail.com>
2024-01-23 04:02:59 +0530

    Commit

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { BsArrowLeft } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
-// import Video from '../assets/'
+import ReactPlayer from 'react-player'
 
 export default function Player(){
     const navigate=useNavigate();
@@ -12,10 +12,28 @@ export default function Player(){
             <div className="back">
                 <BsArrowLeft onClick={() => navigate(-1)} />
             </div>
-            {/* <video src={video} autoPlay loop controls muted></video> */}
+            <ReactPlayer className="video" url="https://www.youtube.com/watch?v=HhesaQXLuRY" playing loop controls />
         </div>
     </Container>
     )
 }
 
-const Container=styled.div``;
+const Container=styled.div`
+    .player {
+        height: 100vh;
+        width: 100vw;
+        .back {
+            position: absolute;
+            padding: 2rem;
+            z-index: 1;
+            svg {
+                font-size: 3rem;
+                cursor: pointer;
+            }
+        }
+        .video {
+            height: 100vh !important;
+            width: 100vw !important;
+        }
+    }
+`;
