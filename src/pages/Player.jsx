731916b6ab0967e37b259d9a35e0e6658242f00b
Aditya-Nagpal <adityanagpal10@gmail.com>
2024-01-23 01:16:00 +0530

    Commit

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import { BsArrowLeft } from 'react-icons/bs'
+import video from '../assets/Avengers-trailer.mp4'
+import { useNavigate } from 'react-router-dom'
 
+export default function Player(){
+    const navigate=useNavigate();
+    return (
+    <Container>
+        <div className="player">
+            <div className="back">
+                <BsArrowLeft onClick={() => navigate(-1)} />
+            </div>
+            <video src={video} autoPlay loop controls muted></video>
+        </div>
+    </Container>
+    )
+}
+
+const Container=styled.div``;
